b391f95dc18f3267d069771be8c2e0c20fd722fd
Tamil <tamil63200@gmail.com>
2023-05-02 12:33:33 +0530

    profile and register and payment design

--- app/src/main/java/com/app/realjobs/fragment/PaymentFragment.java
+++ app/src/main/java/com/app/realjobs/fragment/PaymentFragment.java
@@ -1,5 +1,7 @@
 package com.app.realjobs.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.app.realjobs.R;
+import com.app.realjobs.activities.PaymentActivity;
 
 
 public class PaymentFragment extends Fragment {
+
+    Button btnUpload;
 
 
 
@@ -21,11 +27,24 @@ public class PaymentFragment extends Fragment {
 
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        btnUpload = root.findViewById(R.id.btnUpload);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), PaymentActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         return root;
     }
